5efd03cfdc35d47fd71923f0c8371f78002f6de0
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-06-19 16:19:30 -0700

    Create NavList.js

---
+++ src/components/navigation/NavList.js
@@ -0,0 +1,30 @@
+import Link from "next/link";
+
+export default function NavList() {
+  return (
+    <>
+      <ul className={`${styles.list} hidden-sm-md`}>
+        <li>
+          <Link href="/" className={styles.link}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link href="/headphones" className={styles.link}>
+            Headphones
+          </Link>
+        </li>
+        <li>
+          <Link href="/speakers" className={styles.link}>
+            Speakers
+          </Link>
+        </li>
+        <li>
+          <Link href="/earphones" className={styles.link}>
+            Earphones
+          </Link>
+        </li>
+      </ul>
+    </>
+  );
+}
