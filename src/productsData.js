c1a1b23dcc9c1b1fd49fdc18806e9ccc42b244d5
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-06-19 04:54:39 -0700

    Create getProductData function

--- src/productsData.js
+++ src/productsData.js
@@ -664,4 +664,15 @@ const productsArray = [
   },
 ];
 
-export { productsArray };
+function getProductData(id) {
+  let productData = productsArray.find((product) => product.id === id);
+
+  if (productData == undefined) {
+    console.log("Product data does not exist for ID: " + id);
+    return undefined;
+  }
+
+  return productData;
+}
+
+export { productsArray, getProductData };
