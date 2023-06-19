073bec123fcc0196960ebb9126b84d40a1beb727
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-06-19 16:28:50 -0700

    Import NavList and add nav logo

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,7 +1,25 @@
+import Image from "next/image";
+import Link from "next/link";
+import NavList from "./NavList";
 export default function Navigation(props) {
   return (
     <>
         <nav className={styles.nav}>
+          {/* MD/LG SCREENS NAVIGATION */}
+          <NavList />
+          {/* NAV LOGO */}
+          <Link className={styles.linkComponent} href="/">
+            <div className={styles.logoContainer}>
+              <Image
+                className={styles.logo}
+                src="/assets/shared/desktop/logo.svg"
+                alt="Audiophile brand logo"
+                width="0"
+                height="0"
+                sizes="100vw"
+              />
+            </div>
+          </Link>
         </nav>
     </>
   );
