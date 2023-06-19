8ade01a94ec30af088c490a9b3d3f9e73f446b55
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-06-19 16:38:06 -0700

    Import Shop component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Navigation from "@/components/navigation/Navigation";
 import Header from "@/components/header/Header";
+import Shop from "@/components/shop/Shop";
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
       <Navigation indexNav="indexNav" />
       <Header />
       <main className="main">
+        <Shop />
       </main>
     </>
   );
