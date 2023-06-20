9c116380943a38f37f303dbdf7ac29b3843a0d2c
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-06-19 20:16:21 -0700

    Create TextInput formik component

--- src/components/checkout/CheckoutForm.js
+++ src/components/checkout/CheckoutForm.js
@@ -1,5 +1,30 @@
 import { Formik, Field, Form, useField } from "formik";
 import * as Yup from "yup";
+const TextInput = ({ label, ...props }) => {
+  const [field, meta] = useField(props);
+  return (
+    <>
+      <label
+        className={
+          meta.touched && meta.error ? styles.labelInvalid : styles.label
+        }
+        htmlFor={props.id || props.name}
+      >
+        {label}
+      </label>
+      <br />
+      <input
+        id={props.name}
+        className={meta.touched && meta.error ? "input-invalid" : ""}
+        {...field}
+        {...props}
+      />
+      {meta.touched && meta.error ? (
+        <div className={styles.error}>{meta.error}</div>
+      ) : null}
+    </>
+  );
+};
 export default function CheckoutForm(props) {
   return (
     <>
