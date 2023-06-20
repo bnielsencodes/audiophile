843e115ce9c74e4fc43a6522e97a3ede16ca5ad3
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-06-19 18:05:11 -0700

    Import and map through AlsoLikeElement

--- src/components/also-like/AlsoLike.js
+++ src/components/also-like/AlsoLike.js
@@ -1,8 +1,15 @@
+import AlsoLikeElement from "@/components/also-like/AlsoLikeElement";
+
 export default function AlsoLike(props) {
+  const alsoLikeElements = props.product.others.map((item) => {
+    return <AlsoLikeElement key={item.id} item={item} />;
+  });
+
   return (
     <>
       <section className={styles.alsoLike}>
         <h5 className={styles.heading}>You May Also Like</h5>
+        <div className={styles.container}>{alsoLikeElements}</div>
       </section>
     </>
   );
