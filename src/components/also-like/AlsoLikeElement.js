737fc5d6a4063dd540653e335ae5db040b2963cc
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-06-19 18:03:50 -0700

    Create AlsoLikeElement.js

---
+++ src/components/also-like/AlsoLikeElement.js
@@ -0,0 +1,43 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function AlsoLikeElement(props) {
+  return (
+    <>
+      <div className={styles.container}>
+        <Image
+          className={`${styles.img} hidden-md`}
+          src={props.item.image.mobile}
+          alt={props.item.name}
+          width="0"
+          height="0"
+          sizes="100vw"
+        />
+        <Image
+          className={`${styles.img} hidden-sm-lg`}
+          src={props.item.image.tablet}
+          alt={props.item.name}
+          width="0"
+          height="0"
+          sizes="100vw"
+        />
+        <Image
+          className={`${styles.img} hidden-sm-md`}
+          src={props.item.image.desktop}
+          alt={props.item.name}
+          width="0"
+          height="0"
+          sizes="100vw"
+        />
+        <h5 className={styles.heading}>{props.item.name}</h5>
+        <Link href={props.item.path}>
+          <input
+            className="btn btn--orange btn-text--white"
+            type="button"
+            value="See Product"
+          />
+        </Link>
+      </div>
+    </>
+  );
+}
