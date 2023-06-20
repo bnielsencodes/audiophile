ec2da4b8076df7bd48136e643459bd14b40eee23
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-06-19 17:12:29 -0700

    Create FeaturedYX1.js

---
+++ src/components/featured-products/FeaturedYX1.js
@@ -0,0 +1,49 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function FeaturedYX1() {
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.imgContainer}>
+          <Image
+            className={`${styles.img} hidden-md`}
+            src="/assets/home/mobile/image-earphones-yx1.jpg"
+            alt="YX1 Earphones"
+            width="0"
+            height="0"
+            sizes="100vw"
+          />
+          <Image
+            className={`${styles.img} hidden-sm-lg`}
+            src="/assets/home/tablet/image-earphones-yx1.jpg"
+            alt="YX1 Earphones"
+            width="0"
+            height="0"
+            sizes="100vw"
+          />
+          <Image
+            className={`${styles.img} hidden-sm-md`}
+            src="/assets/home/desktop/image-earphones-yx1.jpg"
+            alt="YX1 Earphones"
+            width="0"
+            height="0"
+            sizes="100vw"
+          />
+        </div>
+        <div className={styles.container2}>
+          <div className={styles.yx1}>
+            <h4 className={styles.heading}>YX1 Earphones</h4>
+            <Link className={styles.link} href="/earphones/yx1">
+              <input
+                className="btn btn--border btn--yx1"
+                type="button"
+                value="See Product"
+              />
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
