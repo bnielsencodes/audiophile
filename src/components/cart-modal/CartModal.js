34f1485be32b3c8fa08c6ae02469ddacbbc595b1
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-06-19 18:42:45 -0700

    Use CartContext

--- src/components/cart-modal/CartModal.js
+++ src/components/cart-modal/CartModal.js
@@ -1,9 +1,18 @@
+import { useContext } from "react";
+import { CartContext } from "../../CartContext.js";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDollarSign } from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
 import styles from "./CartModal.module.css";
 
 export default function Cart(props) {
+  const cart = useContext(CartContext);
+  // get quantity of total items in cart
+  const productsCount = cart.items.reduce(
+    (sum, product) => sum + product.quantity,
+    0
+  );
+
   return (
     <>
       <div className={styles.cartOverlay}>
