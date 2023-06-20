a9b7e80c11e8fbe898108854508e1d6eb4447b40
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-06-19 18:03:29 -0700

    Create AlsoLike.js

---
+++ src/components/also-like/AlsoLike.js
@@ -0,0 +1,9 @@
+export default function AlsoLike(props) {
+  return (
+    <>
+      <section className={styles.alsoLike}>
+        <h5 className={styles.heading}>You May Also Like</h5>
+      </section>
+    </>
+  );
+}
