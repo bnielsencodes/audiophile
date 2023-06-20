00418a88f57035cad9d55c4875a07d9c70de361d
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-06-19 19:15:07 -0700

    Create SummaryItem.js

---
+++ src/components/checkout/SummaryItem.js
@@ -0,0 +1,34 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDollarSign } from "@fortawesome/free-solid-svg-icons";
+import Image from "next/image";
+
+export default function SummaryItem(props) {
+  return (
+    <>
+      <div className={styles.item}>
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.img}
+            src={productData.cartImage}
+            alt="product"
+            width="0"
+            height="0"
+            sizes="100vw"
+          />
+        </div>
+        <div className={styles.productDetails}>
+          <div className={styles.nameQuantityContainer}>
+            <h6>{productData.shortName}</h6>
+            <p className={styles.quantity}>x{quantity}</p>
+          </div>
+          <div className={styles.priceContainer}>
+            <div className={styles.iconContainer}>
+              <FontAwesomeIcon icon={faDollarSign} />
+            </div>
+            <p>{productData.price}</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
