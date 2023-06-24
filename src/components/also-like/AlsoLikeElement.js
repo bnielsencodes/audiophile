2f251f77698c5240a46314b0b0cdf3718358bddf
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-06-24 00:50:10 -0700

    Fix img height issue
    Image height auto was causing images and headings to not render. Putting images into a container and setting height auto on the container worked.

--- src/components/also-like/AlsoLikeElement.js
+++ src/components/also-like/AlsoLikeElement.js
@@ -6,30 +6,33 @@ export default function AlsoLikeElement(props) {
   return (
     <>
       <div className={styles.container}>
-        <Image
-          className={`${styles.img} hidden-md`}
-          src={props.item.image.mobile}
-          alt={props.item.name}
-          width="0"
-          height="0"
-          sizes="100vw"
-        />
-        <Image
-          className={`${styles.img} hidden-sm-lg`}
-          src={props.item.image.tablet}
-          alt={props.item.name}
-          width="0"
-          height="0"
-          sizes="100vw"
-        />
-        <Image
-          className={`${styles.img} hidden-sm-md`}
-          src={props.item.image.desktop}
-          alt={props.item.name}
-          width="0"
-          height="0"
-          sizes="100vw"
-        />
+        <div className={styles.imgContainer}>
+          <Image
+            className={`${styles.img} hidden-md`}
+            src={props.item.image.mobile}
+            alt={props.item.name}
+            width="0"
+            height="0"
+            sizes="100vw"
+          />
+          <Image
+            className={`${styles.img} hidden-sm-lg`}
+            src={props.item.image.tablet}
+            alt={props.item.name}
+            width="0"
+            height="0"
+            sizes="100vw"
+          />
+          <Image
+            className={`${styles.img} hidden-sm-md`}
+            src={props.item.image.desktop}
+            alt={props.item.name}
+            width="0"
+            height="0"
+            sizes="100vw"
+          />
+        </div>
+
         <h5 className={styles.heading}>{props.item.name}</h5>
         <Link href={props.item.path}>
           <input
