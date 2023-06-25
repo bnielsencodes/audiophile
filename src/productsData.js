4fe2c3e5af8c938217a1ab17769b7789e2731397
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-06-25 15:30:21 -0700

    Update product prices
    Default product prices with the challenge's designs were too unrealistic.

--- src/productsData.js
+++ src/productsData.js
@@ -24,8 +24,8 @@ const productsArray = [
         "/assets/product-xx99-mark-two-headphones/desktop/image-category-page-preview.jpg",
     },
     new: true,
-    price: 2999,
-    priceString: "2,999",
+    price: 379,
+    priceString: "379",
     description:
       "The new XX99 Mark II headphones is the pinnacle of pristine audio. It redefines your premium headphone experience by reproducing the balanced depth and precision of studio-quality sound.",
     features:
@@ -144,8 +144,8 @@ const productsArray = [
         "/assets/product-xx99-mark-one-headphones/desktop/image-category-page-preview.jpg",
     },
     new: false,
-    price: 1750,
-    priceString: "1,750",
+    price: 329,
+    priceString: "329",
     description:
       "As the gold standard for headphones, the classic XX99 Mark I offers detailed and accurate audio reproduction for audiophiles, mixing engineers, and music aficionados alike in studios and on the go.",
     features:
@@ -256,8 +256,8 @@ const productsArray = [
         "/assets/product-xx59-headphones/desktop/image-category-page-preview.jpg",
     },
     new: false,
-    price: 899,
-    priceString: "899",
+    price: 239,
+    priceString: "239",
     description:
       "Enjoy your audio almost anywhere and customize it to your specific tastes with the XX59 headphones. The stylish yet durable versatile wireless headset is a brilliant companion at home or on the move.",
     features:
@@ -359,8 +359,8 @@ const productsArray = [
         "/assets/product-zx9-speaker/desktop/image-category-page-preview.jpg",
     },
     new: true,
-    price: 4500,
-    priceString: "4,500",
+    price: 1199,
+    priceString: "1,199",
     description:
       "Upgrade your sound system with the all new ZX9 active speaker. It’s a bookshelf speaker system that offers truly wireless connectivity -- creating new possibilities for more pleasing and practical audio setups.",
     features:
@@ -467,8 +467,8 @@ const productsArray = [
         "/assets/product-zx7-speaker/desktop/image-category-page-preview.jpg",
     },
     new: false,
-    price: 3500,
-    priceString: "3,500",
+    price: 899,
+    priceString: "899",
     description:
       "Stream high quality sound wirelessly with minimal to no loss. The ZX7 speaker uses high-end audiophile components that represents the top of the line powered speakers for home or studio use.",
     features:
@@ -576,8 +576,8 @@ const productsArray = [
         "/assets/product-yx1-earphones/desktop/image-category-page-preview.jpg",
     },
     new: true,
-    price: 599,
-    priceString: "599",
+    price: 299,
+    priceString: "299",
     description:
       "Tailor your listening experience with bespoke dynamic drivers from the new YX1 Wireless Earphones. Enjoy incredible high-fidelity sound even in noisy environments with its active noise cancellation feature.",
     features:
