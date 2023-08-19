077016884ddfaa295164b7ff852b9128b19b436b
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-08-18 18:29:22 -0700

    Create Counter.js

---
+++ src/components/counter/Counter.js
@@ -0,0 +1,52 @@
+import { useContext } from "react";
+import { CartContext } from "../../CartContext.js";
+import styles from "./Counter.module.css";
+
+export default function ProductDetails(props) {
+  const product = props.product;
+  const cart = useContext(CartContext);
+  const productQuantity = cart.getProductQuantity(props.id);
+
+  return (
+    <>
+      <div
+        className={`${styles.quantityContainer} 
+        ${
+          props.class == "productDetails"
+            ? styles["quantityContainer--productDetails"]
+            : props.class === "cartItem"
+            ? styles["quantityContainer--cartItem"]
+            : null
+        }`}
+      >
+        <div className={styles.removeContainer}>
+          <p
+            className={styles.removeBtn}
+            onClick={() => cart.removeOneFromCart(props.id)}
+          >
+            &minus;
+          </p>
+        </div>
+        <p className={styles.quantity}>{productQuantity}</p>
+        <div className={styles.addContainer}>
+          <p
+            className={styles.addBtn}
+            onClick={() => cart.addOneToCart(props.id)}
+          >
+            &#43;
+          </p>
+        </div>
+      </div>
+      {props.class === "productDetails" && (
+        <div>
+          <input
+            className="add-to-cart btn btn--orange btn-text--white"
+            onClick={() => cart.deleteFromCart(product.id)}
+            type="button"
+            value="Remove"
+          />
+        </div>
+      )}
+    </>
+  );
+}
